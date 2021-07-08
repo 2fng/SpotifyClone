37f3934212c64db22f5d9b682d52dbf58a254f95
Hua Son Tung <tunghuason@gmail.com>
2021-07-08 13:02:26 +0700

    Initial Commit

---
+++ SpotifyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Hua Son Tung on 08/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
